7bf3502305c10d0b0b621bd10bad0130767bf63b
Steve Bour <bourpower@gmail.com>
2022-02-19 13:35:45 -0500

    initial JS and SQL written

---
+++ server.js
@@ -0,0 +1,35 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        port: PORT,
+        user: "root",
+        password: "Luckygadfly530!!",
+        database: "employee_db"
+    },
+    console.log(`Connected to Employee Tracker Database on port ${PORT}`),
+    init()
+);
+
+function init() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you list to do?",
+            name: "slection",
+            choices: [
+                "View All Employees",
+                "View All Employees by Role",
+                "View All Employees by Department",
+                "Update Employee",
+                "Add Employee",
+                "Add Role",
+                "Add Department"
+            ]
+        }
+    ])
+};
